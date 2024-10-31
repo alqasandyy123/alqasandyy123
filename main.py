6d3103bff91ca5d1c445755ccce7ea79aa74b796
alqasandyy123 <alqasandyyy212@gmail.com>
2024-10-31 23:10:45 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# input user
+
+# data yang dimasukan
+data = input("masukan data : ")
+
+# cetak
+print("data =",data,"type :",type(data))
+
+# supaya tidak string lakukan casting
+angka = float(input("masukan angka: "))
+print("data :",angka,"type :",type(angka))
